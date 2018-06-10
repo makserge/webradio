53e398c8154b7b68ab4394a49917d703118ddde3
Sergey Makogon <smakogon@luxoft.com>
2018-06-11 00:31:47 +0300

    Fixed mode mapping

--- backend-nanopi-neo/react-native/WebRadio/src/constants/Common.js
+++ backend-nanopi-neo/react-native/WebRadio/src/constants/Common.js
@@ -1,3 +1,11 @@
+export const WEB_RADIO_ROUTE_INDEX = 0;
+export const FM_RADIO_ROUTE_INDEX = 1;
+export const AUDIO_PLAYER_ROUTE_INDEX = 2;
+export const BLUETOOH_ROUTE_INDEX = 3;
+export const AIRPLAY_ROUTE_INDEX = 4;
+export const LINE_IN_ROUTE_INDEX = 5;
+export const SETTINGS_ROUTE_INDEX = 6;
+
 export const MEDIA_NOTIFICATION_ID = '1';
 export const SLEEP_TIMER_NOTIFICATION_ID = '2';
 export const SERVER_HOST = 'serverHost';
--- backend-nanopi-neo/react-native/WebRadio/src/middleware/Navigation.js
+++ backend-nanopi-neo/react-native/WebRadio/src/middleware/Navigation.js
@@ -5,6 +5,15 @@ import { NavigationActions } from 'react-navigation';
 import PushNotification from 'react-native-push-notification';
 
 import { getMode } from '../actions/AppState';
+import {
+  WEB_RADIO_ROUTE_INDEX,
+  FM_RADIO_ROUTE_INDEX,
+  AUDIO_PLAYER_ROUTE_INDEX,
+  BLUETOOH_ROUTE_INDEX,
+  AIRPLAY_ROUTE_INDEX,
+  LINE_IN_ROUTE_INDEX,
+  SETTINGS_ROUTE_INDEX,
+} from '../constants/Common';
 
 const REMOVE_NOTIFICATION_DELAY = 7000; // 7 sec
 
@@ -12,38 +21,53 @@ const onChangeNavigationScene = () => {
   setTimeout(() => PushNotification.cancelAllLocalNotifications(), REMOVE_NOTIFICATION_DELAY);
 };
 
+const mapModeToRoute = (mode) => {
+  switch (mode) {
+    case WEB_RADIO_ROUTE_INDEX:
+      return 'WebRadio';
+    case FM_RADIO_ROUTE_INDEX:
+      return 'FmRadio';
+    case AUDIO_PLAYER_ROUTE_INDEX:
+      return 'AudioPlayer';
+    case BLUETOOH_ROUTE_INDEX:
+      return 'Bluetooth';
+    case AIRPLAY_ROUTE_INDEX:
+      return 'AirPlay';
+    case LINE_IN_ROUTE_INDEX:
+      return 'LineIn';
+    default:
+      return 'WebRadio';
+  }
+};
+
 const navigation = createReactNavigationReduxMiddleware(
   'root',
   state => state.navigation,
 );
 
-const getCurrentRouteName = (navigationState) => {
+const getCurrentRouteIndex = (navigationState) => {
   if (!navigationState) {
     return null;
   }
-  const route = navigationState.routes[navigationState.index];
-  if (route.routes) {
-    return getCurrentRouteName(route);
-  }
-  return route.routeName;
+  return navigationState.index;
 };
 
 const screenTracking = store => next => (action) => {
   const currentState = store.getState();
-  const currentScreen = getCurrentRouteName(currentState.navigation);
+  const currentScreen = getCurrentRouteIndex(currentState.navigation);
   const currentMode = currentState.appState.mode;
   const result = next(action);
   const newState = store.getState();
-  const nextScreen = getCurrentRouteName(newState.navigation);
+  const nextScreen = getCurrentRouteIndex(newState.navigation);
   const newMode = newState.appState.mode;
 
   if ((action.type === 'redux-pouchdb/SET_REDUCER' && action.reducer === 'AppState') && (newMode !== currentMode)) {
-    store.dispatch(NavigationActions.navigate({ routeName: newMode }));
+    store.dispatch(NavigationActions.navigate({ routeName: mapModeToRoute(newMode) }));
   } else if ((action.type === NavigationActions.NAVIGATE) && (nextScreen !== currentScreen)) {
     onChangeNavigationScene();
-    if (nextScreen !== 'Settings') {
+    if (nextScreen !== SETTINGS_ROUTE_INDEX) {
       store.dispatch(getMode(nextScreen));
-    }  
+    }
   }
   return result;
 };
